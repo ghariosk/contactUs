0eaa24b3d97946e6d07eee2fae65852b8648e0ec
ghariosk <karlgharios@hotmail.com>
2019-09-23 19:53:30 +0300

    test

--- contactUs/lambda.js
+++ contactUs/lambda.js
@@ -1,11 +1,11 @@
 let AWS = require('aws-sdk');
 const ses = new AWS.SES();
 
-exports.handler = function async (event, context, callback) {
+exports.handler = function async(event, context, callback) {
     console.log(event)
 
-    var {who,email,name,phoneNumber,city} = event.body
-    var data 
+    var { who, email, name, phoneNumber, city } = event.body
+    var data
     if (who == "user") {
         data = `${name} wants to know more about Wazifa. You can reach them at ${email} or ${phoneNumber}. He lives in ${city}`
     } else {
@@ -30,18 +30,18 @@ exports.handler = function async (event, context, callback) {
         },
         Source: 'Wazifa No Reply <no-reply@wazifa.solutions>',
     }, function (err, data) {
-        console.log(err,data)
-            const response = {
+        console.log(err, data)
+        const response = {
             statusCode: 200,
-           headers: {
-            'Access-Control-Allow-Origin' : '*',
-            'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-            'Access-Control-Allow-Credentials' : true,
-            'Content-Type': 'application/json'
-        },
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                'Access-Control-Allow-Credentials': true,
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({ "message": "Success" })
-            };
-        if (err) callback(null, response ); // an error occurred
+        };
+        if (err) callback(null, { "message": "Error" }); // an error occurred
         else callback(null, response);           // successful response
     });
 }
